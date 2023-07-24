28b148c23e7b73d6bd4111511b6e2260c2ce4e06
salem.affa <salem.affa@mehriso.com>
2023-07-24 17:02:25 +0100

    chore: handle decimal numbers

--- dist/lib.d.ts
+++ dist/lib.d.ts
@@ -2,4 +2,3 @@ export declare function unite(nombre: number): string;
 export declare function dizaine(nombre: number): string;
 export declare function NumberToLetter(nombre: number): string;
 export declare function FloatToLetter(nombre: number, precision?: number): string;
-//# sourceMappingURL=lib.d.ts.map
